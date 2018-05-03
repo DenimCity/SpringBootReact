8c5c4c6852f9927bad3978d3ffa590ba20c1fd3e
DenimCity <altidorj1@gmail.com>
2018-05-03 12:04:24 -0400

    post route works teseting delete route

--- demo/src/main/java/com/example/demo/services/SpannerService.java
+++ demo/src/main/java/com/example/demo/services/SpannerService.java
@@ -2,11 +2,15 @@ package com.example.demo.services;
 
 import com.example.demo.model.Door;
 import com.example.demo.util.DbSpannerService;
-import com.google.cloud.spanner.*;
+import com.google.cloud.spanner.Key;
+import com.google.cloud.spanner.Mutation;
+import com.google.cloud.spanner.ResultSet;
+import com.google.cloud.spanner.Statement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -29,29 +33,24 @@ public class SpannerService {
         }
         return doors;
     }
-//
-//    public  Door insertDoor(Door door){
-//        List<Mutation> mutations = new ArrayList<>();
-//        mutations.add(
-//            Mutation.newInsertBuilder("Door")
-//            .set("door_id")
-//            .to(door.getId())
-//            .set("door_name")
-//            .to(door.getName())
-//            .build());
-//
-//        SpannerOptions options = SpannerOptions.newBuilder().build();
-//        Spanner spanner = options.getService();
-//        DatabaseClient dbClient = spanner.getDatabaseClient(DatabaseId.of(
-//                project, instance, database));
-//
-//        dbClient.write(mutations);
-//        return door;
-//    }
-//
-    public  Door selectDoor(Integer doorId){
-        List<Door> doors = new ArrayList<>();
 
+    public  Door insertDoor(Door door){
+
+        //creating a new door
+        List<Mutation> mutations = new ArrayList<>();
+        mutations.add(
+            Mutation.newInsertBuilder("Door")
+            .set("door_id")
+            .to(door.getId())
+            .set("door_name")
+            .to(door.getName())
+            .build());
+
+        dbSpannerService.getDBConnection().write(mutations);
+        return door;
+    }
+
+    public  Door selectDoor(Integer doorId){
         // Queries the database for one dooor
         Door door = new Door();
         ResultSet resultSet = dbSpannerService.getDBConnection().singleUse().executeQuery(Statement.of("SELECT * from Door where door_id="+doorId));
@@ -62,23 +61,23 @@ public class SpannerService {
         return door;
 
     }
-//
-//
-//    public static Door deleteDoor(Integer doorId){
-//        SpannerOptions options = SpannerOptions.newBuilder().build();
-//        Spanner spanner = options.getService();
-//        DatabaseClient dbClient = spanner.getDatabaseClient(DatabaseId.of(
-//                project, instance, database));
-//
-//        Door door = new Door();
-//        ResultSet resultSet = dbClient.singleUse().executeQuery(Statement.of("SELECT * from Door where door_id="+doorId));
-//        while(resultSet.next()){
-//            door.setId((int)resultSet.getLong("door_id"));
-//            door.setName(resultSet.getString("door_name"));
-//        }
-//        return door.remove(doorId);
-//
-//    }
+
+    public  void deleteDoor(Integer doorId){
+
+
+try {
+    dbSpannerService.getDBConnection().writeAtLeastOnce(Arrays.asList(Mutation.delete("Door", Key.of(doorId))));
+
+} catch (Exception e) {
+    LOGGER.log("Delete didnt wor," , e);
+}
+
+
+//                (Statement.of("DELETE from Door where door_id="+doorId));
+
+
+
+    }
 
 
 }

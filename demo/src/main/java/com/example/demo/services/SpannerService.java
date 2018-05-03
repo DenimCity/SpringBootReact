899aa7a5f6f52bfe871e7b159682da931412fa1c
DenimCity <altidorj1@gmail.com>
2018-05-03 11:33:36 -0400

    get individual door by iD spanner service now works

--- demo/src/main/java/com/example/demo/services/SpannerService.java
+++ demo/src/main/java/com/example/demo/services/SpannerService.java
@@ -1,60 +1,60 @@
 package com.example.demo.services;
 
 import com.example.demo.model.Door;
+import com.example.demo.util.DbSpannerService;
 import com.google.cloud.spanner.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class SpannerService {
 
-    static String project = "firstapp-202814", instance = "firstapp", database= "firstapp-db";
+    @Autowired
+    DbSpannerService dbSpannerService;
 
+    @Autowired
+    public SpannerService(DbSpannerService dbSpannerService) {
+        this.dbSpannerService = dbSpannerService;
+    }
 
-    public static List<Door> getAllDoors(){
+    public List<Door> getAllDoors(){
         List<Door> doors = new ArrayList<>();
-
-        SpannerOptions options = SpannerOptions.newBuilder().build();
-        Spanner spanner = options.getService();
-        DatabaseClient dbClient = spanner.getDatabaseClient(DatabaseId.of(
-                project, instance, database));
         // Queries the database
-        ResultSet resultSet = dbClient.singleUse().executeQuery(Statement.of("SELECT * from door"));
+        ResultSet resultSet = dbSpannerService.getDBConnection().singleUse().executeQuery(Statement.of("SELECT * from door"));
         while(resultSet.next()){
             doors.add(new Door( (int) resultSet.getLong("door_id"), resultSet.getString("door_name")));
         }
         return doors;
     }
+//
+//    public  Door insertDoor(Door door){
+//        List<Mutation> mutations = new ArrayList<>();
+//        mutations.add(
+//            Mutation.newInsertBuilder("Door")
+//            .set("door_id")
+//            .to(door.getId())
+//            .set("door_name")
+//            .to(door.getName())
+//            .build());
+//
+//        SpannerOptions options = SpannerOptions.newBuilder().build();
+//        Spanner spanner = options.getService();
+//        DatabaseClient dbClient = spanner.getDatabaseClient(DatabaseId.of(
+//                project, instance, database));
+//
+//        dbClient.write(mutations);
+//        return door;
+//    }
+//
+    public  Door selectDoor(Integer doorId){
+        List<Door> doors = new ArrayList<>();
 
-    public static Door insertDoor(Door door){
-        List<Mutation> mutations = new ArrayList<>();
-        mutations.add(
-            Mutation.newInsertBuilder("Door")
-            .set("door_id")
-            .to(door.getId())
-            .set("door_name")
-            .to(door.getName())
-            .build());
-
-        SpannerOptions options = SpannerOptions.newBuilder().build();
-        Spanner spanner = options.getService();
-        DatabaseClient dbClient = spanner.getDatabaseClient(DatabaseId.of(
-                project, instance, database));
-
-        dbClient.write(mutations);
-        return door;
-    }
-
-    public static Door selectDoor(Integer doorId){
-//        List<Door> doors = new ArrayList<>();
-
-        SpannerOptions options = SpannerOptions.newBuilder().build();
-        Spanner spanner = options.getService();
-        DatabaseClient dbClient = spanner.getDatabaseClient(DatabaseId.of(
-                project, instance, database));
-        // Queries the database
+        // Queries the database for one dooor
         Door door = new Door();
-        ResultSet resultSet = dbClient.singleUse().executeQuery(Statement.of("SELECT * from Door where door_id="+doorId));
+        ResultSet resultSet = dbSpannerService.getDBConnection().singleUse().executeQuery(Statement.of("SELECT * from Door where door_id="+doorId));
         while(resultSet.next()){
             door.setId((int)resultSet.getLong("door_id"));
             door.setName(resultSet.getString("door_name"));
@@ -62,6 +62,23 @@ public class SpannerService {
         return door;
 
     }
+//
+//
+//    public static Door deleteDoor(Integer doorId){
+//        SpannerOptions options = SpannerOptions.newBuilder().build();
+//        Spanner spanner = options.getService();
+//        DatabaseClient dbClient = spanner.getDatabaseClient(DatabaseId.of(
+//                project, instance, database));
+//
+//        Door door = new Door();
+//        ResultSet resultSet = dbClient.singleUse().executeQuery(Statement.of("SELECT * from Door where door_id="+doorId));
+//        while(resultSet.next()){
+//            door.setId((int)resultSet.getLong("door_id"));
+//            door.setName(resultSet.getString("door_name"));
+//        }
+//        return door.remove(doorId);
+//
+//    }
 
 
 }

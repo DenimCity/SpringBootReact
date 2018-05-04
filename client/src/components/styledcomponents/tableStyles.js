9dce3d8da168603070514f0097533921d9088070
DenimCity <altidorj1@gmail.com>
2018-05-04 09:45:08 -0400

    updated table styles

--- client/src/components/styledcomponents/tableStyles.js
+++ client/src/components/styledcomponents/tableStyles.js
@@ -8,13 +8,14 @@ flex-direction:column;
 `
 export const Table = styled.div `
    border: 1px black solid;
-    padding: 2px;
+    padding: 5px;
     display: flex;
     color: black;
     justify-content: center;
     background-color: lightgray;
     color: black;
-    width: 12vw;
+    width: 100%;
+    
 `
 
 export const Column = styled.div `
@@ -23,6 +24,8 @@ export const Column = styled.div `
 export const ColumnTitle = styled.div `
     padding-bottom: 1.5vh;
     padding-right: 1.2vw;
+    text-align:left;
+    rowsspan:2;
 `
 
 export const Holder = styled.div `
